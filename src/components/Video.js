d268214f2125bc56c55d772c62c92b10c2cc6163
Soumyajit Pathak <drenther@users.noreply.github.com>
2018-08-24 02:13:42 +0530

    Added README

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,24 +3,16 @@ import React, { Component } from "react";
 class Video extends Component {
   videoRef = React.createRef();
 
-  componentDidMount() {
-    // video autoplay without user interaction throws error in Safari
-    try {
-      this.videoRef.current.play();
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
+  // Safari won't allow playing the video without user interaction
+  componentDidUpdate() {
     const video = this.videoRef.current;
     if (this.props.active) video.play();
-    if (!this.props.active) video.pause();
+    else video.pause();
   }
 
   render() {
     return (
-      <video ref={this.videoRef}>
+      <video autoPlay ref={this.videoRef}>
         <source type="video/mp4" src={this.props.src} />
       </video>
     );
